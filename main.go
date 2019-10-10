b807772d5e02d4e3486346e59a3f2e08d2028ca6
yunus <yunus>
2019-10-10 20:21:07 +0000

    Отдельная функция для чтения настроек

--- main.go
+++ main.go
@@ -16,26 +16,36 @@ type config struct {
 func main() {
 	journal.Info("application started")
 
-	// Вычитывание настроек.
-	journal.Info("reading config file")
-	f, err := os.Open("config.json")
+	cfg, err := readConfig("config.json")
 	if err != nil {
 		journal.Fatal(err)
 	}
-	configRaw, err := ioutil.ReadAll(f)
-	f.Close()
+
+	// Горутина для пополнения БД фильмами.
+	journal.Info("starting movies fetch goroutine")
+	go theMovieDBHarvester(cfg.TheMovieDBKey, cfg.DBName)
+	select {}
+}
+
+func readConfig(cfgFilename string) (*config, error) {
+	journal.Info("reading config file")
+
+	f, err := os.Open(cfgFilename)
 	if err != nil {
-		journal.Fatal(err)
+		return nil, err
+	}
+	defer f.Close()
+	configRaw, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
 	}
 	var cfg config
 	err = json.Unmarshal(configRaw, &cfg)
 	if err != nil {
-		journal.Fatal(err)
+		return nil, err
 	}
-	journal.Info("config file read OK")
 
-	// Горутина для пополнения БД фильмами.
-	journal.Info("starting movies fetch goroutine")
-	go theMovieDBCrawler(cfg.TheMovieDBKey, cfg.DBName)
-	select {}
+	journal.Info("config file read OK")
+	return &cfg, nil
+
 }
