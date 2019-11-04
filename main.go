b8abfeb5498c09f43a0788fa0caf5b3612107b27
yunus <yunus>
2019-11-04 19:04:19 +0000

    Отдельный файл для работы с настройками

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"bot/journal"
-	"encoding/json"
-	"io/ioutil"
-	"os"
 )
 
 func main() {
@@ -22,41 +19,4 @@ func main() {
 	go bot(&cfg.Bot, cfg.DBName)
 
 	select {}
-}
-
-type botConfig struct {
-	Token       string `json:"telegram_token"`
-	WebhookAddr string `json:"webhook_address"`
-	WebhookPort int    `json:"webhook_port"`
-	BotAPIAddr  string `json:"telegram_bot_api_address"`
-	PublicCert  string `json:"public_cert"`
-	PrivateKey  string `json:"private_key"`
-}
-
-type config struct {
-	TheMovieDBKey string    `json:"themoviedb_key"`
-	DBName        string    `json:"db_name"`
-	Bot           botConfig `json:"bot_config"`
-}
-
-func readConfig(cfgFilename string) (*config, error) {
-	journal.Info("reading config file")
-
-	f, err := os.Open(cfgFilename)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	configRaw, err := ioutil.ReadAll(f)
-	if err != nil {
-		return nil, err
-	}
-	var cfg config
-	err = json.Unmarshal(configRaw, &cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	journal.Info("config file read OK")
-	return &cfg, nil
 }
