ac9373a45ac69ffa56ad6eeeb8246928048f5e28
Yunus <yunus@fastmail.com>
2020-01-12 23:39:02 +0300

    Добавил скрипт сборки бота

---
+++ build.sh
@@ -0,0 +1,15 @@
+SQLITE_LOCATION=sqlite/lib
+
+echo "building SQLite library..."
+pushd $SQLITE_LOCATION 1>/dev/null
+gcc -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_DEFAULT_FOREIGN_KEYS=1 -c sqlite3.c
+ar rcs libsqlite3.a sqlite3.o
+rm sqlite3.o
+popd 1>/dev/null
+echo "SQLite library build finished"
+
+echo "building movie-promo-bot"
+go build movie-promo-bot
+echo "movie-promo-bot build finished"
+
+rm $SQLITE_LOCATION/libsqlite3.a
--- sqlite/lib/build.sh
+++
@@ -1,3 +0,0 @@
-gcc -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_DEFAULT_FOREIGN_KEYS=1 -c sqlite3.c
-ar rcs libsqlite3.a sqlite3.o
-rm sqlite3.o
